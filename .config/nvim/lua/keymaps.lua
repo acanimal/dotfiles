64a4cdbb5b1b02def2f114735f48880c5d4157af
acanimal <asantiagop@gmail.com>
2023-10-12 19:44:50 +0200

    added desc comments for keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -5,13 +5,13 @@ vim.g.maplocalleader = ' '
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, { desc = "Diagnostic open float" })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Diagnostic previous" })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Diagnostic next" })
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, { desc = "Diagnostic show local list" })
 vim.diagnostic.config({
   update_in_insert = true
 })
 
-vim.keymap.set('n', '//', '<cmd>nohl<cr>') -- Remove the search highlight
+vim.keymap.set('n', '//', '<cmd>nohl<cr>', { desc = 'Remove highlight search mark' }) -- Remove the search highlight
 
