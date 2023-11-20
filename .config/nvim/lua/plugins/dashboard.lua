5b1c9ac557d9bdb3c3d1ddf0aa48de402450d474
acanimal <asantiagop@gmail.com>
2023-11-20 08:27:06 +0100

    updated options

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,11 +1,10 @@
 return {
   'glepnir/dashboard-nvim',
-  -- event = 'VimEnter',
-  dependencies = { {'nvim-tree/nvim-web-devicons'}},
+  event = 'VimEnter',
+  dependencies = { { 'nvim-tree/nvim-web-devicons' } },
   config = function()
-    -- require('dashboard').setup {
-    --   shortcut_type = 'number'
-    -- }
+    require('dashboard').setup {
+      shortcut_type = 'number'
+    }
   end,
 }
-
