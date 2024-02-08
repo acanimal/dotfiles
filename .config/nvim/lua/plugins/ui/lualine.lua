69042ea08a7bb79d47be42e9af53f0686c974f60
acanimal <asantiagop@gmail.com>
2024-02-08 15:06:04 +0100

    Updated to catpuccino theme

--- .config/nvim/lua/plugins/ui/lualine.lua
+++ .config/nvim/lua/plugins/ui/lualine.lua
@@ -1,15 +1,24 @@
 return {
   "nvim-lualine/lualine.nvim", -- statusline
-	dependencies = {
-		'nvim-tree/nvim-web-devicons'
-	},
- 	config = function()
-		require("lualine").setup{
-			options = {
-				theme = "tokyonight"
-        -- thene = "kanagawa"
-			}
-		}
-	end,
+  dependencies = {
+    'nvim-tree/nvim-web-devicons'
+  },
+  opts = {
+    winbar = {
+      lualine_a = { 'buffers' },
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {}
+    },
+    inactive_winbar = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { 'buffers' },
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {}
+    }
+  }
 }
-
--- .config/nvim/lua/plugins/ui/theme.lua
+++ .config/nvim/lua/plugins/ui/theme.lua
@@ -1,12 +1,20 @@
+-- return {
+--   "folke/tokyonight.nvim",
+--   -- "rebelot/kanagawa.nvim",
+--   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--   priority = 1000, -- make sure to load this before all the other start plugins
+--   config = function()
+--     -- load the colorscheme here
+--     vim.cmd("colorscheme tokyonight-night")
+--     -- vim.cmd("colorscheme kanagawa-wave")
+--   end
+-- }
+
 return {
-  "folke/tokyonight.nvim",
-  -- "rebelot/kanagawa.nvim",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
   config = function()
-    -- load the colorscheme here
-    vim.cmd("colorscheme tokyonight-night")
-    -- vim.cmd("colorscheme kanagawa-wave")
+    vim.cmd("colorscheme catppuccin-macchiato")
   end
 }
-
