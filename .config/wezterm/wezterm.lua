e49af14afa4e30a3954360e0d74461da9374990b
acanimal <asantiagop@gmail.com>
2024-05-17 17:11:54 +0200

    Updated color for inactive term panes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm'
+local action = wezterm.action
 local config = {}
 
 config.color_scheme = 'Batman'
@@ -9,5 +10,22 @@ config.hide_mouse_cursor_when_typing = true
 config.initial_rows = 40
 config.initial_cols = 180
 config.switch_to_last_active_tab_when_closing_tab = true
+
+-- Config movement keys
+config.keys = {
+  -- Move word by word
+  { mods = "OPT", key = "LeftArrow",  action = action.SendKey({ mods = "ALT", key = "b" }) },
+  { mods = "OPT", key = "RightArrow", action = action.SendKey({ mods = "ALT", key = "f" }) },
+  -- Move line begining and end
+  { mods = "CMD", key = "LeftArrow",  action = action.SendKey({ mods = "CTRL", key = "a" }) },
+  { mods = "CMD", key = "RightArrow", action = action.SendKey({ mods = "CTRL", key = "e" }) },
+  { mods = "CMD", key = "Backspace",  action = action.SendKey({ mods = "CTRL", key = "u" }) },
+}
+
+-- Change color for inactive panes
+config.inactive_pane_hsb = {
+  saturation = 0.3,
+  brightness = 0.3,
+}
 
 return config
--- .zshrc
+++ .zshrc
@@ -105,16 +105,17 @@ source $ZSH/oh-my-zsh.sh
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 # Add dotfiles alias configuration
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 # Add doomemacs to the path
-export PATH="$HOME/.emacs.doom.d/bin:$PATH"
+export PATH="$HOME/.config/emacs/bin:$PATH"
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 export PATH="/usr/local/sbin:$PATH"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
